77c5d6f0e54bf216a2f9bc1b16386dc65d653452
kattakapilkumar <kapilkatta123@gmail.com>
2017-08-30 19:04:45 +0530

    datastructues using java

---
+++ usingJAVA/src/HelloDataStructures.java
@@ -0,0 +1,11 @@
+
+public class HelloDataStructures
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+           System.out.println("Hello DataStructures");
+	}
+
+}
---
+++ usingJAVA/src/linkedlist/HelloLinkedList.java
@@ -0,0 +1,13 @@
+package linkedlist;
+
+public class HelloLinkedList 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello LinkedList");
+
+	}
+
+}
