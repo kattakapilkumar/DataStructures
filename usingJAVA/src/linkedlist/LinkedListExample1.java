3259a4f66d1ab47ba74a69258cb22b48beeb2c47
kattakapilkumar <kapilkatta123@gmail.com>
2017-10-05 21:25:49 +0530

    commit

---
+++ usingJAVA/src/linkedlist/LinkedListExample1.java
@@ -0,0 +1,14 @@
+package linkedlist;
+
+public class LinkedListExample1 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
---
+++ usingJAVA/src/linkedlist/NodeCreation.java
@@ -0,0 +1,30 @@
+package linkedlist;
+
+public class NodeCreation 
+{
+
+	Node head;   //object reference
+	class Node 
+	{
+		int data;   //datatype
+		Node next;  //here next is a OBJECT REFERENCE
+		
+		
+		//here is the constructor
+		Node(int d)
+		{
+			//here first line = 
+			//we can use  Node(int d)  and this.data=data;
+			data = d;
+			
+			next=null;
+		}
+		
+		
+		
+		//to start the linkedlist  head is required
+				
+		
+	}
+
+}
