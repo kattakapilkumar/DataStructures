ff097163fc2c7fc371d7b163dd8a71c9f41665d4
Kapil Kumar Katta <kapilkatta123@gmail.com>
2017-10-06 20:13:00 +0530

    linkedlist

--- usingJAVA/src/linkedlist/LinkedListExample1.java
+++ usingJAVA/src/linkedlist/LinkedListExample1.java
@@ -3,16 +3,48 @@ package linkedlist;
 public class LinkedListExample1 
 {
 	Node head;   //object reference
+	/*public void push(int new_data)
+	{
+		Node new_node_obj = new Node(new_data);
+		
+		
+		
+		new_node_obj.next = head;  //pointing to HEAD...
+		
+		
+		head = new_node_obj;  // making the inserted new data node which is inserted in the front as HEAD
+		
+	}*/
+	
+	
+	
+	static Node justobj;
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
 		LinkedListExample1  obj=new LinkedListExample1();
 		
 		
+		
+		Node new_dataobj = new Node(4);
 		obj.head = new Node(1);
 		Node secondobj = new Node(2);
 		Node thirdobj = new Node(3);		
 		obj.head.next = secondobj;
-		secondobj.next = thirdobj;
+		secondobj.next = thirdobj;	
+		
+		justobj.push(6);
+		
+		obj.printList();
+		
 	}
+	public void printList()
+    {
+        Node n = head;
+        while (n != null)
+        {
+            System.out.print(n.data+" ");
+            n = n.next;
+        }
+    }
 }
--- usingJAVA/src/linkedlist/Node_Insertion_FRONT.java
+++ usingJAVA/src/linkedlist/Node_Insertion_FRONT.java
@@ -1,24 +1,8 @@
 package linkedlist;
 
-public class Node 
+public class Node_Insertion_FRONT extends LinkedListExample1
 {
-
-	
 	Node head;
-	int data;   //datatype
-	Node next;  //here next is a OBJECT REFERENCE	
-	//here is the constructor
-	Node(int d)
-	{
-		//here first line = 
-		//we can use  Node(int d)  and this.data=data;
-		data = d;
-		
-		next=null;
-	}
-	
-	
-	
 	public void push(int new_data)
 	{
 		Node new_node_obj = new Node(new_data);
@@ -31,5 +15,6 @@ public class Node
 		head = new_node_obj;  // making the inserted new data node which is inserted in the front as HEAD
 		
 	}
+	
 	
 }
